19984868bd403f26248ec892bb0ed0de6af12d2b
williamforbrigd <hansw0701@gmail.com>
2021-03-01 16:33:31 +0100

    added post_timeout()

--- exercise4/Workers.hpp
+++ exercise4/Workers.hpp
@@ -5,7 +5,6 @@
 #include <mutex>
 #include <thread>
 #include <vector>
-#include <atomic>
 
 using namespace std;
 
@@ -14,34 +13,24 @@ private:
   condition_variable cv;
   mutex task_mutex;
   mutex finished_mutex;
+  mutex wait_mutex;
   list<function<void()>> tasks;
   int no_threads;
   bool is_running = false;
-  atomic<bool> finished{false};
+  bool finished = false;
+  bool wait;
   vector<thread> threads;
 
 public:
-  Workers(bool start_threads) : is_running(start_threads) {
-    if(start_threads) 
-      start(); 
-  }
   Workers(int no_threads_) : no_threads(no_threads_) {}
 
-  ~Workers() {
-    cout << "Destructor is called." << endl;
-  }
+  ~Workers() { cout << "Destructor is called." << endl; }
 
-  template<typename... Args> 
-  void post(Args&&... args) {
+  template <typename... Args> void post(Args &&...args) {
     {
       unique_lock<mutex> lock(task_mutex);
       tasks.emplace_back(bind(forward<Args>(args)...));
     }
-    {
-        //unique_lock<mutex> lock(wait_mutex);
-        //wait = false;
-    }
-    //cv.notify_all();
     cv.notify_one();
   }
 
@@ -54,46 +43,68 @@ public:
   }
 
   void run_threads() {
-    for(int i=0; i < no_threads; i++) {
+    for (int i = 0; i < no_threads; i++) {
       threads.emplace_back([this] {
-        for(;;) {
-            function<void()> task;
-            {
-              unique_lock<mutex> lock(task_mutex);
-              if(!tasks.empty()) {
-                task = *tasks.begin();
-                tasks.pop_front();
-              } else {
-                if(finished) return;
-                cv.wait(lock);
-              }
+        for (;;) {
+          {
+            unique_lock<mutex> lock(wait_mutex);
+            while (wait)
+              cv.wait(lock);
           }
-          if(task) {
+          function<void()> task;
+          {
+            unique_lock<mutex> lock(task_mutex);
+            if (!tasks.empty()) {
+              task = *tasks.begin();
+              tasks.pop_front();
+            } else {
+              if (finished)
+                return;
+              else
+                cv.wait(lock);
+            }
+          }
+          if (task) {
             task();
-          } 
+          }
         }
       });
     }
   }
 
- void join() {
-   stop();
-   for(auto &t : threads) {
-     t.join();
-   }
- }
+  void join() {
+    stop();                   // Calls stop() which stops all the threads
+    for (auto &t : threads) { // joins the threads after they are stopped
+      t.join();
+    }
+  }
 
- void stop() {
-   finished.exchange(true);
-   cv.notify_all();
- }
+  void stop() {
+    {
+      unique_lock<mutex> lock(task_mutex);
+      finished = true;
+      wait = false;
+    }
+    cv.notify_all();
+  }
 
- void post_tasks(int length, int &count, mutex &count_mutex) {
-   for(int i=0; i < length; i++) {
-     post([&count, &count_mutex]{
-       unique_lock<mutex> lock(count_mutex);
-       count++;
-     });
-   }
- }
+  void post_timeout(function<void()> task, int ms) {
+    {
+      unique_lock<mutex> lock(task_mutex);
+      tasks.emplace_back([task, ms] {
+        this_thread::sleep_for(chrono::milliseconds(ms));
+        task();
+      });
+    }
+  }
+
+  // Method for testing purposes.
+  void post_tasks(int length, int &count, mutex &count_mutex) {
+    for (int i = 0; i < length; i++) {
+      post([&count, &count_mutex] {
+        unique_lock<mutex> lock(count_mutex);
+        count++;
+      });
+    }
+  }
 };
--- exercise4/main.cpp
+++ exercise4/main.cpp
@@ -1,3 +1,4 @@
+
 #include "Workers.hpp"
 #include <iostream>
 #include <thread>
@@ -10,38 +11,30 @@ int main() {
 
   worker_threads.start(); // Create 4 internal threads.
   event_loop.start();     // Create 1 internal thread.
-  mutex count_mutex;
-  int count = 0;
 
-  worker_threads.post_tasks(12356, ref(count), ref(count_mutex));
-  event_loop.post_tasks(644, ref(count), ref(count_mutex));
+  // mutex count_mutex;
+  // int count = 0;
+  // worker_threads.post_tasks(12356, ref(count), ref(count_mutex));
+  // event_loop.post_tasks(644, ref(count), ref(count_mutex));
 
-/*
-  worker_threads.post([] {
-    //cout << "Task A running in thread " << this_thread::get_id() << endl;
-    cout << "Task A" << endl;
-  });
+  worker_threads.post([] { cout << "Task A" << endl; });
 
-  worker_threads.post([] {
-    //cout << "Task B running in thread " << this_thread::get_id() << endl;
-    cout << "Task B" << endl;
-  });
+  worker_threads.post([] { cout << "Task B" << endl; });
 
-  event_loop.post([] {
-    //cout << "Task C running in thread " << this_thread::get_id() << endl;
-    cout << "Task C" << endl;
-  });
+  event_loop.post([] { cout << "Task C" << endl; });
 
-  event_loop.post([] {
-    //cout << "Task D running in thread " << this_thread::get_id() << endl;
-    cout << "Task D" << endl;
-  });
-  */
+  event_loop.post([] { cout << "Task D" << endl; });
 
-  worker_threads.join(); //Calls join() on the worker threads
-  event_loop.join(); //Calls join() on the event loop
+  // The task is executed after 3 seconds.
+  worker_threads.post_timeout([] { cout << "Task E" << endl; }, 3000);
 
-  cout << "The count is: " << count << endl;
+  // The tasks is executed after 3 seconds.
+  event_loop.post_timeout([] { cout << "Task F" << endl; }, 3000);
+
+  worker_threads.join(); // Calls join() on the worker threads
+  event_loop.join();     // Calls join() on the event loop
+
+  // cout << "The count is: " << count << endl;
 
   return 0;
 }
