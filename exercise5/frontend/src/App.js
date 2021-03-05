d6e4a2e135024c3fe9adbaad560f253c9b686623
williamforbrigd <hansw0701@gmail.com>
2021-03-05 15:18:29 +0100

    added frontendE

---
+++ exercise5/frontend/src/App.js
@@ -0,0 +1,10 @@
+import './App.css';
+import Compile from './components/Compile';
+
+function App() {
+  return (
+    <Compile />
+  );
+}
+
+export default App;
---
+++ exercise5/frontend/src/components/Compile.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Compile() {
+
+    return (
+        <div>
+            <button
+                onClick={console.log("hva skjer")}
+            >
+                Press me
+            </button>
+        </div>
+    );
+}
+export default Compile;
